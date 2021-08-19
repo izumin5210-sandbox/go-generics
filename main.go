d22763aeffb0bfa62018193e36df62db069323c8
izumin5210 <m@izum.in>
2021-08-20 08:40:17 +0900

    Add funcs

--- main.go
+++ main.go
@@ -3,15 +3,138 @@ package main
 import "fmt"
 
 func main() {
+	mapSliceTest()
+	filterSliceTest()
+	containsSliceTest()
+	compactSliceTest()
+	uniqSliceTest()
+	differenceSliceTest()
+}
+
+func mapSliceTest() {
+	fmt.Println("## mapSlice")
 	fmt.Println(
+		"double elements",
 		mapSlice([]int{1, 2, 3}, func(v int) int {
 			return v * 2
 		}),
 	)
 
 	fmt.Println(
+		"length of each strings",
 		mapSlice([]string{"foobar", "baz"}, func(v string) int {
 			return len(v)
 		}),
 	)
+
+	fmt.Println()
+}
+
+func filterSliceTest() {
+	fmt.Println("## fliterSlice")
+	fmt.Println(
+		"extract even numbers",
+		filterSlice([]int{1, 2, 3, 4, 5, 6}, func(v int) bool {
+			return v/2 == 0
+		}),
+	)
+
+	type User struct {
+		ID    int
+		Name  string
+		Admin bool
+	}
+
+	users := []*User{
+		{ID: 1, Name: "Alice"},
+		{ID: 2, Name: "Bob", Admin: true},
+		{ID: 3, Name: "Carol"},
+	}
+	adminUsers := filterSlice(
+		users,
+		func(user *User) bool { return user.Admin },
+	)
+	fmt.Println(
+		"extract admin users ",
+		mapSlice(
+			adminUsers,
+			func(user *User) string { return user.Name },
+		),
+	)
+
+	nonAdminUsers := filterNotSlice(
+		users,
+		func(user *User) bool { return user.Admin },
+	)
+	fmt.Println(
+		"extract non-admin users ",
+		mapSlice(
+			nonAdminUsers,
+			func(user *User) string { return user.Name },
+		),
+	)
+
+	fmt.Println()
+}
+
+func containsSliceTest() {
+	fmt.Println("## containsSlice")
+	fmt.Println(
+		"contains 4",
+		containsSlice([]int{1, 5, 6}, 4),
+	)
+
+	fmt.Println()
+}
+
+func compactSliceTest() {
+	fmt.Println("## compactSlice")
+	fmt.Println(
+		"compact strings",
+		compactSlice([]string{"foo", "bar", "", "baz", ""}),
+	)
+
+	type User struct {
+		ID   int
+		Name string
+	}
+
+	fmt.Println(
+		"compact structs",
+		mapSlice(
+			compactSlice([]User{{ID: 1, Name: "Alice"}, {}, {ID: 2, Name: "Bob"}}),
+			func(user User) string { return user.Name },
+		),
+	)
+
+	// compiler panics
+	// fmt.Println(
+	//   "compact structs",
+	//   mapSlice(
+	//     compactSlice([]*User{{ID: 1, Name: "Alice"}, nil, {ID: 2, Name: "Bob"}}),
+	//     func(user *User) string { return user.Name },
+	//   ),
+	// )
+
+	fmt.Println()
+}
+
+func uniqSliceTest() {
+	fmt.Println("## uniqSlice")
+	fmt.Println(
+		`uniq(["a", "a", "b", "b", "c"]) =`,
+		uniqSlice([]string{"a", "a", "b", "b", "c"}),
+	)
+
+	fmt.Println()
+}
+
+func differenceSliceTest() {
+	fmt.Println("## differenceSlice")
+	fmt.Println(
+		"[1, 1, 2, 2, 3, 3, 4, 5] - [1, 2, 4] =",
+		differenceSlice([]int{1, 1, 2, 2, 3, 3, 4, 5}, []int{1, 2, 4}),
+	)
+
+	fmt.Println()
 }
--- slices.go
+++ slices.go
@@ -7,3 +7,79 @@ func mapSlice[In any, Out any](inputs []In, mapFunc func(input In) Out) []Out {
 	}
 	return outputs
 }
+
+func filterSlice[In any](inputs []In, filterFunc func(input In) bool) []In {
+	outputs := make([]In, 0, len(inputs))
+	for _, input := range inputs {
+		if filterFunc(input) {
+			outputs = append(outputs, input)
+		}
+	}
+	return outputs
+}
+
+func filterNotSlice[In any](inputs []In, filterFunc func(input In) bool) []In {
+	return filterSlice(inputs, func(input In) bool { return !filterFunc(input) })
+}
+
+type comparableInterface[T any] interface {
+	Compare(a, b T) string
+}
+
+func containsSlice[In comparable](inputs []In, want In) bool {
+	for _, input := range inputs {
+		if input == want {
+			return true
+		}
+	}
+	return false
+}
+
+func compactSlice[In comparable](inputs []In) []In {
+	var zero In
+	return filterNotSlice(inputs, func(input In) bool { return input == zero })
+}
+
+func uniqSlice[In comparable](inputs []In) []In {
+	set := makeSet[In](len(inputs))
+	return filterSlice(inputs, func(input In) bool {
+		if set.Contains(input) {
+			return false
+		}
+		set.Add(input)
+		return true
+	})
+}
+
+func differenceSlice[In comparable](inputs []In, values []In) []In {
+	set := setFromSlice(values)
+	return filterNotSlice(inputs, func(input In) bool {
+		return set.Contains(input)
+	})
+}
+
+func makeSet[T comparable](capacity int) set[T] {
+	values := make(map[T]struct{}, capacity)
+	return set[T]{values: values}
+}
+
+func setFromSlice[T comparable](slice []T) set[T] {
+	values := make(map[T]struct{}, len(slice))
+	for _, v := range slice {
+		values[v] = struct{}{}
+	}
+	return set[T]{values: values}
+}
+
+type set[T comparable] struct {
+	values map[T]struct{}
+}
+
+func (s set[T]) Add(t T) {
+	s.values[t] = struct{}{}
+}
+
+func (s set[T]) Contains(t T) bool {
+	_, ok := s.values[t]
+	return ok
+}
